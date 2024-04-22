37bfd9d2d1e7d02181036d2a300896938c4bb94b
Bun1379 <trieunam19955@gmail.com>
2024-04-22 21:51:55 +0700

    Nav

--- src/components/User/NavigationBar/NavigationBar.jsx
+++ src/components/User/NavigationBar/NavigationBar.jsx
@@ -1,8 +1,39 @@
 import "./NavigationBar.css";
 import { NavLink } from 'react-router-dom';
-function NavigationBar() {
-  return (
+import { useState } from "react";
 
+const Dropdown = ({ categories }) => {
+  const numCols = 4; // Số cột
+
+  const numRows = Math.ceil(categories.length / numCols); // Số hàng
+
+  const renderGrid = () => {
+    return Array(numRows)
+      .fill()
+      .map((_, rowIndex) => (
+        <div className="dropdown-row" key={rowIndex}>
+          {categories.slice(rowIndex * numCols, (rowIndex + 1) * numCols).map((category, colIndex) => (
+            <div key={colIndex} className="dropdown-item">
+              {category}
+            </div>
+          ))}
+        </div>
+      ));
+  };
+
+  return <div className="dropdown">{renderGrid()}</div>;
+};
+
+function NavigationBar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const categories = ["Action", "Horror", "Fantasy", "Comedy", "Romance"];
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+
+  return (
     <div className="container_options">
       <ul>
         <li>
@@ -21,7 +52,10 @@ function NavigationBar() {
           </NavLink>
         </li>
         <li>
-          <span>Thể loại</span>
+          <div className="category" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+            Thể loại
+            {isDropdownOpen && <Dropdown categories={categories} />}
+          </div>
         </li>
       </ul>
     </div>
