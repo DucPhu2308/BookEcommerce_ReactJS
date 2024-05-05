0ff9e2cf0904bb3475f2b566f0bb01f713eab8ce
DucPhu2308 <nguyenducphu2308@gmail.com>
2024-05-05 20:49:14 +0700

    update seen book section

--- src/pages/User/Home/UpdateBook_SeenBookList/SeenBookList/SeenBookList.jsx
+++ src/pages/User/Home/UpdateBook_SeenBookList/SeenBookList/SeenBookList.jsx
@@ -1,25 +1,51 @@
 import SeenBookItem from "@/components/User/SeenBookItem/SeenBookItem";
-import './SeenBookList.css';
+import "./SeenBookList.css";
 
+import { Link } from "react-router-dom";
 
-const SeenBookList = ({list, max}) => {
-    return (
-        <div className="container_nav_2_seenBooks">
-              <div className="container_nav_2_seenBooks_title">
-                <span>Truyện đã xem</span>
-              </div>
+const SeenBookList = ({ list, max }) => {
+  const token = localStorage.getItem("token");
 
-              <ul>
-                {/* display to maxSeenBooks */}
-                {list.slice(0, max).map((book, index) => (
-                  console.log(book),
-                  <li key={index}>
-                    <SeenBookItem book={book.bookRead} />
-                  </li>
-                ))}
-              </ul>
-            </div>
-    )
-}
+  return (
+    <div className="container_nav_2_seenBooks">
+      <div className="container_nav_2_seenBooks_title">
+        <span>Truyện đã xem</span>
+      </div>
+
+      {/* if user not login */}
+      {!token && (
+        <div className="container_nav_2_seenBooks_message">
+          <span><Link style={{
+            backgroundColor: "#111",
+            color: "#eee",
+          }} to="/login">Đăng nhập</Link> để mở tính năng truyện đã xem</span>
+        </div>
+      )}
+
+      {/* if user login but not seen any book */}
+      {token && list.length === 0 && (
+        <div className="container_nav_2_seenBooks_message">
+          <span>Bạn chưa xem truyện nào</span>
+        </div>
+      )}
+        
+      {/* if user login and seen some books */}
+      {token && list.length > 0 && 
+      (<ul>
+        {/* display to maxSeenBooks */}
+        {list.slice(0, max).map(
+          (book, index) => (
+            console.log(book),
+            (
+              <li key={index}>
+                <SeenBookItem book={book.bookRead} />
+              </li>
+            )
+          )
+        )}
+      </ul>)}
+    </div>
+  );
+};
 
 export default SeenBookList;
