ad73d13dc6ebbbfcd34c457c00748d8c618baad5
DucPhu2308 <nguyenducphu2308@gmail.com>
2024-05-12 21:53:58 +0700

    update css ranking board

--- src/pages/User/Home/RankingList/RankItem.jsx
+++ src/pages/User/Home/RankingList/RankItem.jsx
@@ -10,7 +10,7 @@ const RankItem = ({ rank, book, onClick }) => {
         {rank === 2 && <img className="medal-icon" src={silvermedal} alt="silvermedal" />}
         {rank === 3 && <img className="medal-icon" src={bronzemedal} alt="bronzemedal" />}
         {rank > 3 && <span className="rank-number">{rank}</span>}
-        <span>{book.title}</span>
+        <span className="item-title">{book.title}</span>
         <span className="view-number">{book.views} lượt xem</span>
       </div>
     </Reveal>
